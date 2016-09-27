c392de1e2eefb3ba08a8d275046c2854e479c6b5
michkles <michkles@gmail.com>
2016-09-27 12:04:08 +0100

    rspec test for number 6

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,8 @@
 def fizzbuzz(number)
   if number == 3
     return "fizz"
+  elsif number == 15
+    return "fizzbuzz"
   else number == 5
     return "buzz"
   end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,8 +1,9 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizz" when passed multiple of 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(6)).to eq 'fizz'
   end
 end
 
